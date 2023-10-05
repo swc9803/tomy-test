486c3cb78ba7a3e50646183acb8e5a57b4fb2c1a
swc9803 <swc9803@gmail.com>
2023-10-05 12:24:49 +0900

    i18n 번들 최적화

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "url";
+import VueI18nVitePlugin from "@intlify/unplugin-vue-i18n/vite";
+
 export default defineNuxtConfig({
   build: {
-    transpile: ["swiper", "gsap"],
+    transpile: ["swiper", "gsap", "vue-i18n"],
+  },
+  vite: {
+    plugins: [
+      VueI18nVitePlugin({
+        include: [
+          resolve(dirname(fileURLToPath(import.meta.url)), "./locales/*.json"),
+        ],
+      }),
+    ],
   },
   nitro: {
     devProxy: {

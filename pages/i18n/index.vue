42cef2fabc8a8d83f57e8b3bb38e6d929da3b50c
swc9803 <swc9803@gmail.com>
2023-10-05 12:19:15 +0900

    i18n test

---
+++ pages/i18n/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <p>i18n</p>
+    <h1>{{ $t("hello", { name: "vue-i18n" }) }}</h1>
+    <form>
+      <label for="locale-select">{{ $t("language") }}: </label>
+      <select id="locale-select" v-model="$i18n.locale">
+        <option value="ko">ko</option>
+        <option value="en">en</option>
+      </select>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+}
+</style>

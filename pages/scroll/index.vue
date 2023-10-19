797c5c4f7fbc6dd557d71093bbbbd8ce9a8ed6da
swc9803 <swc9803@gmail.com>
2023-10-19 13:28:30 +0900

    커서 추가 test

--- pages/scroll/index.vue
+++ pages/scroll/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <section>1</section>
-    <section>2</section>
-    <section>3</section>
+    <section>section1</section>
+    <section>section2</section>
+    <section>section3</section>
     <div>
       <div ref="circleRef" class="circle" />
       <svg
@@ -23,24 +23,30 @@
           :style="{ strokeDashoffset: `${dashOffset}px` }"
         />
       </svg>
+
+      <div ref="circleRef2" class="cursor">
+        <div ref="waveRef" class="wave" />
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
 const circleRef = ref();
+const circleRef2 = ref();
+const waveRef = ref();
 const progressIndicatorRef = ref();
 
 const circleLength = ref(2 * Math.PI * 39);
 const dashOffset = ref(circleLength.value);
 
 const onMouseMove = (e) => {
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
-  circleRef.value.style.top = `${mouseY}px`;
-  circleRef.value.style.left = `${mouseX}px`;
-  progressIndicatorRef.value.style.top = `${mouseY}px`;
-  progressIndicatorRef.value.style.left = `${mouseX}px`;
+  circleRef.value.style.top = `${e.clientY}px`;
+  circleRef.value.style.left = `${e.clientX}px`;
+  circleRef2.value.style.top = `${e.clientY}px`;
+  circleRef2.value.style.left = `${e.clientX}px`;
+  progressIndicatorRef.value.style.top = `${e.clientY}px`;
+  progressIndicatorRef.value.style.left = `${e.clientX}px`;
 };
 
 const onScroll = () => {
@@ -49,6 +55,7 @@ const onScroll = () => {
     document.documentElement.clientHeight;
   const scrollTop = window.scrollY || document.documentElement.scrollTop;
 
+  waveRef.value.style.top = `${100 - (scrollTop / height) * 100}%`;
   dashOffset.value = circleLength.value * (1 - scrollTop / height);
 };
 
@@ -65,6 +72,11 @@ onBeforeUnmount(() => {
 <style lang="scss" scoped>
 div {
   section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 4em;
+    color: white;
     height: 100vh;
     &:nth-child(1) {
       background: rgb(66, 66, 66);
@@ -76,19 +88,64 @@ div {
       background: rgb(22, 22, 22);
     }
   }
-  .circle {
+
+  .cursor {
     position: fixed;
-    transform: translate(-50%, -50%);
     width: 60px;
     height: 60px;
+    transform: translate(-150%, -50%);
+    border: 2px solid #fff;
     border-radius: 50%;
-    background: white;
+    background: transparent;
+    overflow: hidden;
+    .wave {
+      position: absolute;
+      top: 100%;
+      left: -50%;
+      width: 200%;
+      height: 200%;
+      border-radius: 38%;
+      // background: linear-gradient(
+      //   rgba(252, 231, 79, 1) 0%,
+      //   rgba(60, 89, 167, 1) 100%
+      // );
+      background: rgb(187, 187, 187);
+      transition: all 0.3s ease;
+      animation: wave 4s linear infinite;
+      &::after {
+        content: "";
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: white;
+        border-radius: 38%;
+        animation: wave 2s linear infinite reverse;
+      }
+    }
+    @keyframes wave {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
   }
+
+  .circle {
+    position: fixed;
+    width: 60px;
+    height: 60px;
+    transform: translate(-550%, -50%);
+    background: white;
+    border-radius: 50%;
+  }
+
   svg {
     position: fixed;
     top: -80px;
     left: -80px;
-    transform: translate(-50%, -50%);
+    transform: translate(-425%, -50%) rotate(-90deg);
   }
 }
 </style>
